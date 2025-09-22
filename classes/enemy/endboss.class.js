96c4cd86a2307a96068b04988e9ed3adfe3523e1
Tim <tim.daubner@outlook.com>
2025-09-22 14:57:35 +0200

    added boss class and mirrored sprites

--- classes/enemy/endboss.class.js
+++ classes/enemy/endboss.class.js
@@ -3,24 +3,25 @@ class EndBoss extends MovableObject {
     height = 128 * 1;
 
     IMAGES_IDLE = [
-        'img/Enemy_Anim/Idle/idle_r_00.png',
-        'img/Enemy_Anim/Idle/idle_r_01.png',
-        'img/Enemy_Anim/Idle/idle_r_02.png',
-        'img/Enemy_Anim/Idle/idle_r_03.png',
-        'img/Enemy_Anim/Idle/idle_r_04.png',
-        'img/Enemy_Anim/Idle/idle_r_05.png'
+        'img/Boss/Idle/idle_r_00.png',
+        'img/Boss/Idle/idle_r_01.png',
+        'img/Boss/Idle/idle_r_02.png',
+        'img/Boss/Idle/idle_r_03.png',
+        'img/Boss/Idle/idle_r_04.png',
+        'img/Boss/Idle/idle_r_05.png',
+        'img/Boss/Idle/idle_r_06.png',
     ];
     IMAGES_WALKING = [
-        'img/Enemy_Anim/Walk/walk_r_00.png',
-        'img/Enemy_Anim/Walk/walk_r_01.png',
-        'img/Enemy_Anim/Walk/walk_r_02.png',
-        'img/Enemy_Anim/Walk/walk_r_03.png',
-        'img/Enemy_Anim/Walk/walk_r_04.png',
-        'img/Enemy_Anim/Walk/walk_r_05.png',
-        'img/Enemy_Anim/Walk/walk_r_06.png',
-        'img/Enemy_Anim/Walk/walk_r_07.png',
-        'img/Enemy_Anim/Walk/walk_r_08.png',
-        'img/Enemy_Anim/Walk/walk_r_09.png',
+        'img/Boss/Walk/walk_r_00.png',
+        'img/Boss/Walk/walk_r_01.png',
+        'img/Boss/Walk/walk_r_02.png',
+        'img/Boss/Walk/walk_r_03.png',
+        'img/Boss/Walk/walk_r_04.png',
+        'img/Boss/Walk/walk_r_05.png',
+        'img/Boss/Walk/walk_r_06.png',
+        'img/Boss/Walk/walk_r_07.png',
+        'img/Boss/Walk/walk_r_08.png',
+        'img/Boss/Walk/walk_r_09.png',
     ];
 
     currentImg = 0;
