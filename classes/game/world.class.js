72c3cd576b1d1f096a7f3c22ae62ab08e4c517f6
studieins <studi.eins@gmx.de>
2025-10-13 07:54:58 +0200

    remove drawFrame()

--- classes/game/world.class.js
+++ classes/game/world.class.js
@@ -192,7 +192,6 @@ class World {
             this.flipImage(mo);
         }
         mo.draw(this.ctx);
-        mo.drawFrame(this.ctx);
 
         if (mo.isMirrored) {
             this.flipImageBack(mo);
